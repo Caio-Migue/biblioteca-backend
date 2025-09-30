e794d1c265090166f6bc7456a067760563485305
Caio-Migue <kazzgenovese293@gmail.com>
2025-09-30 08:45:26 -0300

    .

--- obj/Debug/net8.0/biblioteca-backend.AssemblyInfo.cs
+++ obj/Debug/net8.0/biblioteca-backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("biblioteca-backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+607844d80d1a775ccebad412f6d8da034a1c6802")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+728bec714bc87a2a6c6faec608e90be83eca68ed")]
 [assembly: System.Reflection.AssemblyProductAttribute("biblioteca-backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("biblioteca-backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
